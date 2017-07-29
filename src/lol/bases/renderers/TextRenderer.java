6eb60b8e7ff26985e00090cad6553bf389c3c006
huynq <qhuydtvt@gmail.com>
2017-07-30 00:57:49 +0700

    Added specs

---
+++ src/lol/bases/renderers/TextRenderer.java
@@ -0,0 +1,15 @@
+package lol.bases.renderers;
+
+import lol.bases.Vector2D;
+
+import java.awt.*;
+import java.util.Vector;
+
+/**
+ * Created by huynq on 7/30/17.
+ */
+public class TextRenderer {
+//    public void render(Graphics2D g2d, String text, ) {
+//
+//    }
+}
