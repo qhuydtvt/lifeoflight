553a82973611e9a5b8cf186b854fe181febb2da7
huynq <qhuydtvt@gmail.com>
2017-08-02 05:34:05 +0700

    Added command buffer

--- out/production/LifeOfLight/src/lol/inputs/InputManager.java
+++ out/production/LifeOfLight/src/lol/inputs/InputManager.java
@@ -4,6 +4,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Created by huynq on 7/28/17.
@@ -11,6 +12,8 @@ import java.util.List;
 public class InputManager implements KeyListener {
     public static final InputManager instance = new InputManager();
     public String command;
+    private ArrayList<String> oldCommands;
+    private int oldCommandCurrentIndex;
 
     private List<CommandListener> commandListeners;
 
@@ -21,6 +24,7 @@ public class InputManager implements KeyListener {
     public InputManager() {
         command = "";
         commandListeners = new ArrayList<>();
+        oldCommands = new ArrayList<>();
     }
 
     @Override
@@ -48,6 +52,11 @@ public class InputManager implements KeyListener {
         for (CommandListener cmdListener : commandListeners) {
             cmdListener.onCommandFinished(this.command);
         }
+        oldCommands.add(command);
+        if (oldCommands.size() > 10) {
+            oldCommands.remove(0);
+        }
+        oldCommandCurrentIndex = oldCommands.size();
         setCommand("");
     }
 
@@ -66,6 +75,16 @@ public class InputManager implements KeyListener {
         if (e.getKeyCode() == KeyEvent.VK_ENTER) {
             if (command.length() > 0) {
                 finishCommand();
+            }
+        } else if (e.getKeyCode() == KeyEvent.VK_UP) {
+            if (oldCommandCurrentIndex > 0) {
+                oldCommandCurrentIndex--;
+                setCommand(oldCommands.get(oldCommandCurrentIndex));
+            }
+        } else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+            if (oldCommandCurrentIndex < oldCommands.size() - 1) {
+                oldCommandCurrentIndex++;
+                setCommand(oldCommands.get(oldCommandCurrentIndex));
             }
         }
     }
