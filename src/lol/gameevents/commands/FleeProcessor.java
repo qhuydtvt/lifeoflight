f8a2b99d87efe981072b7c0f2b05161fcfd9ccab
huynq <qhuydtvt@gmail.com>
2017-08-01 07:38:39 +0700

    Added flee

--- src/lol/gameevents/commands/FleeProcessor.java
+++ src/lol/gameevents/commands/FleeProcessor.java
@@ -1,8 +1,16 @@
 package lol.gameevents.commands;
 
+import lol.bases.Utils;
+import lol.events.EventManager;
+import lol.gameentities.State;
+import lol.gameentities.players.Player;
+import lol.gameevents.CombatEvent;
 import lol.gameevents.GameEvent;
+import lol.gameevents.MainGameEvent;
+import lol.monsters.Monster;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by huynq on 8/1/17.
@@ -10,6 +18,23 @@ import java.util.List;
 public class FleeProcessor extends CommandProcessor {
     @Override
     public GameEvent process(List<String> commands, GameEvent currentEvent) {
-        return null;
+        if (Utils.rollDice() < State.instance.getPlayer().getLuck()) {
+            EventManager.pushUIMessage("You escaped from combat");
+            return new MainGameEvent();
+        } else {
+            EventManager.pushUIMessage("You tried to flee but failed");
+            fight(State.instance.getPlayer(), ((CombatEvent)currentEvent).getMonsters());
+            return null;
+        }
+    }
+
+    private void fight(Player player, List<Monster> monsters) {
+        Monster monster = monsters.get(new Random().nextInt(monsters.size()));
+        if(Utils.rollDice() < monster.getStat().getLuck()) {
+            player.getHit(monster.getStat().getStrength());
+            EventManager.pushUIMessage(String.format("%s hit you, you now has %s hp left", monster.getName(), player.getHp()));
+        } else {
+            EventManager.pushUIMessage(String.format("%s attacked you, but missed", monster.getName()));
+        }
     }
 }
